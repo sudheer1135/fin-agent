81c63df44cd3398e1e71c51ac7f01c3bb3de11ba
sudheer1135 <sudheer.g1135@gmail.com>
2025-12-02 18:16:40 +0530

    jupiter1 edited

---
+++ jupiter1.py
@@ -0,0 +1,11 @@
+tar -czvf backup.tar.gz /home/user/documents
+✔ What this does:
+c: creates a new tar archive
+
+z: compresses it using gzip
+
+v: prints each file being added
+
+f: names the output file backup.tar.gz
+
+Archives /home/user/documents
